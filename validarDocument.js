4d8606145f9f19e9f1b7a9dbc08426295be8803c
Julianaf <julianafatsil@mail.ufsm.br>
2019-04-18 21:19:08 -0300

    Inicio validação de documentos

---
+++ validarDocument.js
@@ -0,0 +1,64 @@
+function EhDocWord(extensaoArq){
+    return extensaoArq == 'DOCX';
+}
+
+function EhDocPdf(extensaoArq){
+    return extensaoArq == 'PDF';
+}
+
+function EhDocEpub(extensaoArq){
+    return extensaoArq == 'EPUB';
+}
+
+function ValidarEhDocumentoExtraivel(extensaoArq){
+    return EhDocPdf(extensaoArq) || EhDocWord(extensaoArq) || EhDocEpub(extensaoArq);
+}
+
+function ExtrairExtensao(filename){
+    return  filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2).toUpperCase();
+}
+
+function ExecutarExatracaoArquivo(filename) {
+    let extArq = ExtrairExtensao(filename);
+    
+    if (ValidarEhDocumentoExtraivel(extArq)){
+        console.log('Arquivo OK ' + extArq);
+
+        if(EhDocWord(extArq)){
+
+            ExtratorWord(filename);
+
+        } else if(EhDocPdf(extArq)){
+            
+            console.log('É documento pdf');
+            ExtratorPdf(filename);
+
+        }else if(EhDocEpub(extArq)){
+            console.log('É documento epub');
+        }
+    } else {
+        console.log('Extensão de arquivo é inválida! ' + extArq);
+    }
+}
+
+function ExtratorWord(filename){
+    let extratorDoc = require('docx-extractor');
+
+    extratorDoc.timeCreated(filename, function (ImageData) {
+        console.log(ImageData )
+    });
+}
+
+function ExtratorPdf(filename){
+    const PDFExtract = require('pdf.js-extract').PDFExtract;
+    const pdfExtract = new PDFExtract();
+    const options = {}; 
+
+    pdfExtract.extract(filename, options, (err, data) => {
+        if (err) return console.log(err);
+            console.log(data);
+    });
+}
+
+ExecutarExatracaoArquivo('../MaiconTeste/teste.docx');
+ExecutarExatracaoArquivo('../MaiconTeste/SAMPLE.PDF');
